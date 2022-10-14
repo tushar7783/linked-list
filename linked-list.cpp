45311b42fd8658745dad6f2393313f2af536131f
TUSHAR KUMAR <103430652+tushar7783@users.noreply.github.com>
2022-10-14 15:57:51 +0530

    Create linked-list.cpp

---
+++ linked-list.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+struct node{
+	int data ;
+	node *link;
+};
+node *head= NULL; //global
+
+
+int main()
+{
+   node *ptr=new node(); 
+   ptr->data=2; 
+   ptr->link=NULL;
+   head=ptr; 
+    return 0;
+}
